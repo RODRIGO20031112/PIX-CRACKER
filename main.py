407d8a432a42873ce61e124691a7636aad376ded
RODRIGO20031112 <rodrigo.bianco.carvalho@gmail.com>
2024-06-12 14:41:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup as BSHTML
 import urllib.request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # This will enable CORS for all routes
+CORS(app)
 
-@app.route('/scrape')
+@app.route('/scrape', methods=['GET'])
 def scrape():
-    page = urllib.request.urlopen('https://checkout.perfectpay.com.br/payments/confirm/PPCPMTB5EN91EH?ref=PPA1VA63&urlCampaignCode=PPU38COLE43')
-    soup = BSHTML(page, features="lxml")
+    url = request.args.get('url')
+    if not url:
+        return jsonify({'error': 'No URL provided'}), 400
 
-    image = soup.find('img', alt='pix_qrcode')
-    image_src = image['src'] if image else None
+    try:
+        page = urllib.request.urlopen(url)
+        soup = BSHTML(page, features="lxml")
 
-    qrCode_input = soup.find('input', id='qrCode')
-    qrCode_value = qrCode_input.get('value') if qrCode_input else None
+        image = soup.find('img', alt='pix_qrcode')
+        image_src = image['src'] if image else None
 
-    result = {
-        'imagem_src': image_src,
-        'qrCode_value': qrCode_value
-    }
+        qrCode_input = soup.find('input', id='qrCode')
+        qrCode_value = qrCode_input.get('value') if qrCode_input else None
 
-    return jsonify(result)
+        result = {
+            'imagem_src': image_src,
+            'qrCode_value': qrCode_value
+        }
+
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
